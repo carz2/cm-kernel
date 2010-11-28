b9e5f2c68183f364cbec8fa3cc7b50d5f6f7cfa8
carz2 <carz8500@gmail.com>
2010-11-28 08:47:37 -0700

    add untracked files

---
+++ PSFreedom/config.h
@@ -0,0 +1,21 @@
+/*
+ * config.h -- PS3 Jailbreak payload : configuration file
+ *
+ * Copyright (C) Youness Alaoui (KaKaRoTo)
+ *
+ * This software is distributed under the terms of the GNU General Public
+ * License ("GPL") version 3, as published by the Free Software Foundation.
+ *
+ */
+
+#ifndef PSFREEDOM_CONFIG_H
+#define PSFREEDOM_CONFIG_H
+
+
+#include "pl3/config.h"
+
+//#define DEBUG
+//#define VERBOSE_DEBUG
+
+
+#endif /* PSFREEDOM_CONFIG_H */
